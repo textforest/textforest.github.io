447141d428f814d70af02deeacdb87068ce1d8a0
Kane Hadley <Kane Hadley>
2015-09-01 23:41:34 -0400

    Implemented flattening.

--- js/tree.js
+++ js/tree.js
@@ -9,13 +9,45 @@ function Tree () {
     var _graph = [];
     var _values = {};
 
-    function addNode () {
+    function addNeighbor (neighbor, key) {
+        if (!key) {
+            _graph.push(key);
+            return this;
+        }
+        var node = findNode(key, _graph);
+        if (node.length > 0) {
+            node.push(key);
+        } else {
+            _graph.push(key);
+        }
 
+        return this;
     }
 
     function children () {
 
     }
+
+    function findNode (key, graph) {
+        if (graph.length > 0) {
+            var guess = member(key, graph);
+            if (guess.length > 0) {
+                return guess;
+            } else {
+                return head(graph.filter(function (d) { return d instanceof Array; })
+                    .map(function (d) { return node(key, d); }));
+            }
+        } else {
+            return [];
+        }
+    }
+
+    function keys () {
+        return flatten(_graph);
+    }
+
+    this.addNeighbor = addNeighbor;
+    this.keys = keys;
 
 }
 
@@ -29,6 +61,11 @@ function addNode (graph, node) {
 }
 
 function flatten (array) {
+    if (array instanceof Array) {
+        return array.map(flatten).reduce(function (a, b) { return a.concat(b);});
+    } else {
+        return [array];
+    }
 }
 
 function head (array) { return array[0] || []; }
